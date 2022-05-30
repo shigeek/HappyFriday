f69255a609df56ae7856297e4df3448bbd452dc6
holasoynaoki <naoki13futsal@gmail.com>
2022-05-30 22:35:59 +0900

    tweak

--- Gemfile
+++ Gemfile
@@ -8,3 +8,5 @@ gemspec
 gem "rake", "~> 13.0"
 
 gem 'holiday_jp', '~> 0.7.0'
+
+gem 'activesupport'
--- lib/HappyFriday.rb
+++ lib/HappyFriday.rb
@@ -1,11 +1,38 @@
 # frozen_string_literal: true
 
 require_relative "HappyFriday/version"
+require 'active_support/all'
+require 'holiday_jp'
 
 module HappyFriday
   class Error < StandardError; end
 
-  def self.test
-    "test"
+  def self.happyfriday?(date)
+    happy_friday == HappyFriday.get_happy_friday(date)
+  end
+
+  def self.get_happy_friday(date)
+    last_of_month = Date.new(date.year, date.month, -1)
+
+    case last_of_month.wday
+    when 0..4
+      before_friday_days = last_of_month.wday + 2
+    when 5..6
+      before_friday_days = last_of_month.wday - 5
+    else
+      raise 'ERR: Unpredictable day of the week.'
+    end
+
+    last_friday = last_of_month.ago(before_friday_days.days)
+
+    while HolidayJp.holiday?(last_friday)
+      last_friday = last_friday.ago(1.week)
+    end
+
+    if last_friday.month != last_of_month.month
+      raise 'HappyFriday is no month!'
+    end
+
+    last_friday
   end
 end
