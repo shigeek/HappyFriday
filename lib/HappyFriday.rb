3758941e73781a4dc426d957d5df0a174bafc790
holasoynaoki <naoki13futsal@gmail.com>
2022-06-04 22:07:27 +0900

    Add comment

--- lib/HappyFriday.rb
+++ lib/HappyFriday.rb
@@ -36,6 +36,7 @@ module HappyFriday
     end
 
     # If HappyFriday of that month has passed, get next month.
+    # TODO: refactoring
     if target_date > last_friday
       next_month = last_friday + 1.month
       happy_friday = HappyFriday.get_next_happy_friday(Date.new(next_month.year, next_month.month))
