268d5875c8b5d08f8b0af2d601ef187b153bd502
holasoynaoki <naoki13futsal@gmail.com>
2022-06-07 09:53:06 +0900

    tweak

--- spec/HappyFriday_spec.rb
+++ spec/HappyFriday_spec.rb
@@ -5,32 +5,32 @@ RSpec.describe HappyFriday do
     expect(HappyFriday::VERSION).not_to be nil
   end
 
-  context 'judge HappyFriday' do
-    it 'If the last Friday of the month is a weekday, return true' do
+  context 'Judge HappyFriday' do
+    it 'If the last Friday of the month is a weekday' do
       date = Date.new(2022, 5, 27)
       expect(HappyFriday.happy_friday?(date)).to eq(true)
     end
 
-    it 'If the last Friday of the month is a holiday, return false' do
+    it 'If the last Friday of the month is a holiday' do
       date = Date.new(2022, 4, 29)
       expect(HappyFriday.happy_friday?(date)).to eq(false)
     end
   end
 
-  context 'get HappyFriday' do
-    it 'normal' do
+  context 'Get HappyFriday' do
+    it 'If the last Friday of the month is a weekday' do
       target_date = Date.new(2022, 5, 1)
       predicted_date = Date.new(2022, 5, 27)
       expect(HappyFriday.get_next_happy_friday(target_date)).to eq(predicted_date)
     end
 
-    it 'Months when the last Friday of the month is a holiday.' do
+    it 'If the last Friday of the month is a holiday' do
       target_date = Date.new(2022, 4)
       predicted_date = Date.new(2022, 4, 28)
       expect(HappyFriday.get_next_happy_friday(target_date)).to eq(predicted_date)
     end
 
-    it 'If HappyFriday of that month has passed, get next month.' do
+    it 'If HappyFriday of that month has passed' do
       target_date = Date.new(2022, 4, 30)
       predicted_date = Date.new(2022, 5, 27)
       expect(HappyFriday.get_next_happy_friday(target_date)).to eq(predicted_date)
